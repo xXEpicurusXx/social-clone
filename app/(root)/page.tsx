648ac11ac680c99cf6c96399fa446ad8abdb0759
Cameron Mckeel <117554357+xXEpicurusXx@users.noreply.github.com>
2024-03-16 17:07:32 +0900

    fixed feed title

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,11 +24,11 @@ async function Home({
 
   return (
     <>
-      <h1 className='head-text text-left'>Home</h1>
+      <h1 className='head-text text-left'>Feed</h1>
 
       <section className='mt-9 flex flex-col gap-10'>
         {result.posts.length === 0 ? (
-          <p className='no-result'>No threads found</p>
+          <p className='no-result'>No posts found</p>
         ) : (
           <>
             {result.posts.map((post) => (
